a11f41fdea62e802014b1c6447ce58a5556f710d
Devin Jacobs <devrjacobs@gmail.com>
2015-02-11 21:01:00 -0700

    Added handleInput.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,8 @@
 import Graphics.Gloss (play, white, red, green, Display(InWindow))
 import Graphics.Gloss.Data.Picture
     (Picture, rectangleSolid, thickCircle, translate, pictures, color, rotate, blank)
-import Graphics.Gloss.Interface.Pure.Game (Event)
+import Graphics.Gloss.Interface.Pure.Game
+    (Event (EventKey), KeyState (Up), MouseButton (LeftButton), Key (MouseButton))
 import qualified Data.Map as M
 
 type Board = M.Map (Int, Int) Marker
@@ -28,7 +29,7 @@ getMarker = flip $ M.findWithDefault Blank
 
 renderBoard :: Board -> Picture
 renderBoard b =
-    pictures $ [ translate    0  50   $ rectangleSolid 300   3
+    pictures $ [ translate    0   50  $ rectangleSolid 300   3
                , translate    0 (-50) $ rectangleSolid 300   3
                , translate   50    0  $ rectangleSolid   3 300
                , translate (-50)   0  $ rectangleSolid   3 300
@@ -37,7 +38,19 @@ renderBoard b =
                | x <- [0..2]
                , y <- [0..2] ]
 
+currentPlayer :: Board -> Marker
+currentPlayer b | even markCount = X
+                | otherwise      = O
+    where
+        markCount = length $ filter (/=Blank) [getMarker b (x, y) | x <- [0..2], y <- [0..2]]
+
 handleInput :: Event -> Board -> Board
+handleInput (EventKey (MouseButton LeftButton) Up _ (x, y)) b =
+    if getMarker b (snap x, snap y) == Blank
+    then M.insert (snap x, snap y) (currentPlayer b) b
+    else b
+    where
+        snap = (+1) . max (-1) . min 1 . fromIntegral . floor . (/100) . (+50)
 handleInput _ b = b
 
 step :: Float -> Board -> Board
