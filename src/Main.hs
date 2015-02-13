9e2335632313011262331b3dd175268f0c22b567
Devin Jacobs <devrjacobs@gmail.com>
2015-02-12 22:55:31 -0700

    Completed AI. Cannot be defeated.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 import Graphics.Gloss (play, white, red, green, Display(InWindow))
 import Graphics.Gloss.Data.Picture
-    (Picture, rectangleSolid, thickCircle, translate, pictures, color, rotate, blank)
+    (Picture, rectangleSolid, thickCircle, translate, pictures, color, rotate,
+     blank)
 import Graphics.Gloss.Interface.Pure.Game
-    (Event (EventKey), KeyState (Up), MouseButton (LeftButton), Key (MouseButton))
+    (Event (EventKey), KeyState (Up), MouseButton (LeftButton),
+     Key (MouseButton))
 import qualified Data.Map as M
 import Data.List (maximumBy)
 import Data.Ord (comparing)
@@ -22,7 +24,8 @@ main = play
     step
 
 renderMarker :: Marker -> Picture
-renderMarker X = color red $ rotate 45 $ pictures [rectangleSolid 90 10, rectangleSolid 10 90]
+renderMarker X = color red $ rotate 45 $
+                     pictures [rectangleSolid 90 10, rectangleSolid 10 90]
 renderMarker O = color green $ thickCircle 35 10
 renderMarker _ = blank
 
@@ -36,16 +39,23 @@ renderBoard b =
                , translate   50    0  $ rectangleSolid   3 300
                , translate (-50)   0  $ rectangleSolid   3 300
                ] ++
-               [ translate ((fromIntegral x - 1) * 100) ((fromIntegral y - 1) * 100) $ renderMarker $ getMarker b (x, y)
+               [ translate
+                    ((fromIntegral x - 1) * 100)
+                    ((fromIntegral y - 1) * 100) $
+                    renderMarker $ getMarker b (x, y)
                | x <- [0..2]
                , y <- [0..2] ]
 
 currentPlayer :: Board -> Marker
 currentPlayer b | odd $ length $ availableMoves b = X
-                | otherwise                      = O
+                | otherwise                       = O
 
 availableMoves :: Board -> [(Int, Int)]
-availableMoves b = [(x, y) | x <- [0..2], y <- [0..2], getMarker b (x, y) == Blank]
+availableMoves b = case winner b of Nothing -> [ (x, y)
+                                               | x <- [0..2]
+                                               , y <- [0..2]
+                                               , getMarker b (x, y) == Blank]
+                                    _       -> []
 
 handleInput :: Event -> Board -> Board
 handleInput (EventKey (MouseButton LeftButton) Up _ (x, y)) b =
@@ -53,7 +63,7 @@ handleInput (EventKey (MouseButton LeftButton) Up _ (x, y)) b =
     then M.insert (snap x, snap y) X b
     else b
     where
-        snap = (+1) . max (-1) . min 1 . fromIntegral . floor . (/100) . (+50)
+        snap = (+1) . max (-1) . min 1 . floor . (/100) . (+50)
 handleInput _ b = b
 
 step :: Float -> Board -> Board
@@ -63,22 +73,39 @@ step _ b =
     else b
 
 aiMove :: Board -> Board
-aiMove b = fst . maximumBy (comparing snd) . map (\m -> (m, aiMinMax m False)) $ map (\k -> M.insert k O b) (availableMoves b)
+aiMove b = fst .
+           maximumBy (comparing snd) .
+           map (\m -> (m, aiMinMax m False)) $
+           map (\k -> M.insert k (currentPlayer b) b) (availableMoves b)
 
 aiMinMax :: Board -> Bool -> Int
 aiMinMax board maximize
     | null $ availableMoves board = scoreBoard board
     | otherwise =
-        let moves = map (\k -> aiMinMax (M.insert k O board) (not maximize)) (availableMoves board)
+        let moves = map
+                (\k -> aiMinMax
+                    (M.insert k (currentPlayer board) board)
+                    (not maximize))
+                (availableMoves board)
         in if maximize then maximum moves else minimum moves
 
 allSame :: Eq a => [a] -> Bool
 allSame []     = True
 allSame (x:xs) = all (==x) xs
 
+maybeHead :: [a] -> Maybe a
+maybeHead (x:_) = Just x
+maybeHead _      = Nothing
+
+winner :: Board -> Maybe Marker
+winner b = maybeHead . map head .
+               filter (\xs -> head xs /= Blank && allSame xs) .
+               map (map $ getMarker b) $ rows (3, 3) 3
+
 scoreBoard :: Board -> Int
-scoreBoard b | any allSame $ map (map $ getMarker b) $ rows (3, 3) 3 = -1
-             | otherwise                                             =  0
+scoreBoard b = case winner b of Just X  -> -1
+                                Just O  ->  1
+                                _       ->  0
 
 directions :: [(Int, Int) -> (Int, Int)]
 directions = [ (\(x, y) -> (x+1, y  ))
